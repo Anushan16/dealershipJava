20b0c5cbb8886a44b3caa60eb21365a09fc6f4f5
Anushan R <anushanr@Anushans-Mac-mini.local>
2022-03-03 13:04:08 -0500

    Initial Commit

---
+++ Dealership.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Dealership{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the Java Dealership");
+        System.out.println("- Select option 'a' to buy a car");
+        System.out.println("- Select option 'b' to sell a car");
+
+        String option = scan.next();
+        // scan.nextline produces a String, unable to set this value to char 
+        
+        switch(option) {
+            case "a":
+                System.out.println("\nYou chose 'a' ");
+                System. out.println("\nDo you have insurance? Type 'yes' or 'no'");
+
+                String insurance  = scan.next();
+                    switch(insurance){
+                    case "yes":
+                        // continue code
+                        System. out.println("\nDo you have a license? Type 'yes' or 'no'");
+
+                        String license  = scan.next();
+                        switch(license){
+                        case "yes":
+                        // continue code
+                            System. out.println("\nWhat is your credit score? ");
+
+                            int creditScore  = scan.nextInt();
+                            if (creditScore >= 600){
+                                System.out.println("Sold! Pleasure doing business with you.");
+                            } else {
+                                System.out.println("Sorry, You have not been approved.. ");
+                            }
+                        break;    
+                        case "no":
+                            System.out.println("Sorry, a license is required to purchase a vehicle");
+                        break;
+                        default:
+                            System.out.println("Sorry, that was a invalid choice");
+                        }
+                    break;
+                    case "no":
+                        System.out.println("Sorry, insurance is required to purchase a vehicle");
+                    break;
+                    default:
+                        System.out.println("Sorry, that was a invalid choice");
+                }
+            break;
+            case "b":System.out.println("You chose 'b' "); break;
+            default:
+            System.out.println("Sorry, that was a invalid choice");
+
+        }
+    }
+}
